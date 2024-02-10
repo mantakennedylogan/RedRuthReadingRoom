78664261a1dd792db4d19475c721249465242d35
mantakennedylogan <mantakennedy.logan@gmail.com>
2024-02-10 00:02:44 -0800

    rerender error

--- public/redruthreforged-master/src/Components/Listen/Listen.jsx
+++ public/redruthreforged-master/src/Components/Listen/Listen.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import axios from '../../API/axios'
-import AudioPlayer from 'react-h5-audio-player'
+import ListenBox from './ListenBox';
 
 function Listen() {
   const [vis, setvis] = React.useState(false);
-  const [audioURL, setAudioUrl] = React.useState(null)
+  const [audioURL, setAudioUrl] = React.useState(null);
+  const [audioList, setAudioList] = React.useState([]);
 
   const getSingleAudio = async() => {
+    
     const response = await axios.get('/api/admin/getAudioFile');
     if(response.data === ''){
       console.log(response)
@@ -19,7 +21,27 @@ function Listen() {
       console.log("end");
       setvis(true); 
     } 
+    
   }
+
+  const getMutupleAudio = async() => {
+    const response = await axios.get('/api/admin/getAudioFilesByPrompt');
+    console.log('test');
+    console.log(response.data[24].file_id);
+    setAudioList(response.data)
+    console.log(audioList)
+    setvis(true); 
+    /*
+    const firstAudio = await axios.get('/api/admin/getAudioFile?file_id='+response.data[24].file_id);
+    console.log(firstAudio);
+    const audioBlob = new Blob([new Uint8Array(firstAudio.data.Body.data)], { type: 'audio/wav' });
+    setAudioUrl(URL.createObjectURL(audioBlob));
+    
+  */
+  
+  }
+    
+    //
 
 
 
@@ -33,10 +55,19 @@ function Listen() {
         </>
       ):null)}
 
+      <button onClick={getMutupleAudio}> Get Mutuple</button>
+      {(vis == true ?(
+         
+          <ListenBox audioToAdd= {audioList}></ListenBox>
+        
+      ):null)}
     </>
   )
 }
 /*
-
+<>
+          <audio src={audioURL} type='audio/mpeg' controls></audio>
+          <ListenBox audioToAdd:></ListenBox>
+        </>
 */
 export default Listen
--- public/redruthreforged-server-master/src/server.js
+++ public/redruthreforged-server-master/src/server.js
@@ -260,8 +260,9 @@ server.post('/api/upload/', upload.single('audio'), (req, res) => {
 // Send Audio form S3 to react
 
 server.get('/api/admin/getAudioFile', (req, res) =>{
-    let key = '1707180583513.m4a'
-    console.log("BIG TIME RUSH THING\n\n\n")
+    console.log(req.query.file_id);
+    let carl = req.query.file_id +".m4a";
+    console.log(carl);
 
     const accessKeyId = 'AKIA2WTBG4K3GELKESGS';
       const secretAccessKey = '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';
@@ -276,7 +277,7 @@ server.get('/api/admin/getAudioFile', (req, res) =>{
     const s3 = new AWS.S3();
     
     s3.getObject(
-        { Bucket: Bucket, Key: key, ResponseContentType: 'audio/mpeg'},
+        { Bucket: Bucket, Key: carl, ResponseContentType: 'audio/mpeg'},
         function (error, data) {
           if (error != null) {
             console.log("Failed to retrieve an object: " + error);
@@ -288,6 +289,15 @@ server.get('/api/admin/getAudioFile', (req, res) =>{
         })
 
     
+})
+
+// returns all of the audio file names for a single prompt
+server.get('/api/admin/getAudioFilesByPrompt', (req, res) =>{
+    collection_id = '666' //hard coded for now
+    connection.query('SELECT t_audio_file.* FROM t_audio_file WHERE prompt_id = ?', [collection_id], function (error, results, fields) {
+        if (error) throw error;
+        res.json(results);
+        });
 })
 
 module.exports = server;
